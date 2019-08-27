2d56c57c158b219453e79a71e8041dbd9b91782b
bbrangeo <bbrangeo@gmail.com>
2019-08-27 14:00:38 +0200

    Modification format function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # coding: utf8
-import json
 import platform
+
 import jsonpickle
 
 if platform.system() == 'Linux':
@@ -17,6 +17,7 @@ class MetaModel(object):
         self.type = type
         self.meta_objects = []
 
+    @property
     def __repr__(self):
         return repr(
             {'id': self.id, 'project_id': self.project_id, 'type': self.type, 'meta_objects': self.meta_objects})
@@ -35,14 +36,14 @@ class MetaObject(object):
 
 
 class IfcParse(object):
-    '''
+    """
     classdocs
-    '''
+    """
 
     def __init__(self, ifc_filename):
-        '''
+        """
         Constructor
-        '''
+        """
 
         self.ifc_filename = ifc_filename
         self.ifcfile = ifcopenshell.open(self.ifc_filename)
@@ -50,12 +51,12 @@ class IfcParse(object):
         self.metaModel = MetaModel(id=self.project.Name, project_id=self.project.GlobalId, type=self.project.is_a())
         self.metaObjects = []
 
-    def extractHierarchy(self, objectDefinition):
+    def extract_hierarchy(self, object_definition):
 
-        parentObject = MetaObject(id=objectDefinition.GlobalId, name=objectDefinition.Name,
-                                  type=objectDefinition.is_a(), parent=None)
+        parentObject = MetaObject(id=object_definition.GlobalId, name=object_definition.Name,
+                                  type=object_definition.is_a(), parent=None)
 
-        if str(objectDefinition.is_a()) != "IfcProject":
+        if str(object_definition.is_a()) != "IfcProject":
             self.metaObjects.append(parentObject)
 
         spatialElement = self.ifcfile.by_type("IfcSpatialStructureElement")
@@ -67,17 +68,17 @@ class IfcParse(object):
                                     type=rE.is_a(), parent=element.GlobalId)
                     self.metaObjects.append(mo.__dict__)
 
-        # if hasattr(objectDefinition, 'IsDecomposedBy'):
-        #     relatedObjects = objectDefinition.IsDecomposedBy
+        # if hasattr(object_definition, 'IsDecomposedBy'):
+        #     relatedObjects = object_definition.IsDecomposedBy
         #     for rO in relatedObjects:
-        #         children = self.extractHierarchy(rO)
+        #         children = self.extract_hierarchy(rO)
         #         self.metaObjects.append(children)
         #         # pprint(item.get_info(recursive=True))
 
         return list(self.metaObjects)
 
-    def toJson(self):
-        metaObjects = self.extractHierarchy(self.project)
+    def to_json(self):
+        metaObjects = self.extract_hierarchy(self.project)
         self.metaModel.meta_objects = metaObjects
         f = open('metaModel.json', 'w')
         stored_info = jsonpickle.encode(self.metaModel.__dict__)
@@ -95,4 +96,4 @@ if __name__ == '__main__':
     ifc_filename = args.ifc_filename
 
     projet = IfcParse(ifc_filename=ifc_filename)
-    projet.toJson()
+    projet.to_json()
